ef485dc68da30ee46977bf479dab70df932fc4c5
DuarteMartinho <duartemartinho11@gmail.com>
2022-01-26 09:35:56 +0000

    Added fields for Order model.

--- backend/duarteshop/models.py
+++ backend/duarteshop/models.py
@@ -31,6 +31,15 @@ class BasketItem(models.Model):
 class Order(models.Model):
     id = models.IntegerField(primary_key=True)
     basketId = models.ForeignKey(Basket, on_delete=models.CASCADE)
-    productId = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(APIUser, on_delete=models.CASCADE)
+    addressline1 = models.CharField(max_length=100)
+    addressline2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=40)
+    
+    #TODO: Implement a choices options for the counties
+    county = models.CharField(max_length=15) 
+
+    country = models.CharField(max_length=15)
+    eircode = models.CharField(max_length=7)
     datetimeOrder = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
