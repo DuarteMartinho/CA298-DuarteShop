b0ee166afd9aea82d9539b2e8132d6b2fa5fe6b5
DuarteMartinho <duartemartinho11@gmail.com>
2022-01-27 19:17:21 +0000

    Added products to home page;

--- backend/duarteshop/urls.py
+++ backend/duarteshop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home, name="home")
+   path('', views.home, name="home"),
+   path('product/<int:prodId>', views.product_individual, name="product_individual")
 ]
--- backend/duarteshop/views.py
+++ backend/duarteshop/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
+    products = Product.objects.all()
     args = {
-        'title': 'Hello World',
-        'name':'There!',
-        'names': ['Duarte1', 'Duarte2', 'Duarte3', 'Duarte4'],
+        'title': 'Website Title',
+        'products': products,
     }
     return render(request, 'home.html', args)
+
+def product_individual(request, prodId):
+    product = Product.objects.get(id=prodId)
+    return render(request, 'product_individual.html', {'product': product})
