3b8ecea73dddeb8cd9333efa31b7d05b300c87e7
DuarteMartinho <duartemartinho11@gmail.com>
2022-01-31 14:15:22 +0000

    Added sign up url.

--- backend/duarteshop/urls.py
+++ backend/duarteshop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name="home"),
-   path('product/<int:prodId>', views.product_individual, name="product_individual")
+   path('product/<int:prodId>', views.product_individual, name="product_individual"),
+   path('register/', views.UserSignupView.as_view(), name="register"),
 ]
