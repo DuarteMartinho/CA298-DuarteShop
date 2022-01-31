66da5090171529330b23dbe7f35b9d41c52c40fb
DuarteMartinho <duartemartinho11@gmail.com>
2022-01-31 14:12:57 +0000

    Added sign up view.

--- backend/duarteshop/views.py
+++ backend/duarteshop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth import login, logout
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -14,3 +17,15 @@ def product_individual(request, prodId):
     product = Product.objects.get(id=prodId)
     return render(request, 'product_individual.html', {'product': product})
 
+class UserSignupView(CreateView):
+    model = APIUser
+    form_class = UserSignupForm
+    template_name = 'user_signup.html'
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
